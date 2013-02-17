b96286de127c5a056ce12d0b1af7b2e53d45bd7d
Adam Erb <adam.l.erb@gmail.com>
2013-02-17 01:03:36 +0100

    Modifying views and presenters to implement observer pattern.

---
+++ src/com/tasty/fish/domain/ExpressionEvaluator.java
@@ -0,0 +1,11 @@
+package com.tasty.fish.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adamerb
+ * Date: 17/02/13
+ * Time: 12:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExpressionEvaluator {
+}
---
+++ src/com/tasty/fish/domain/IExpressionEvaluator.java
@@ -0,0 +1,11 @@
+package com.tasty.fish.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adamerb
+ * Date: 17/02/13
+ * Time: 12:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IExpressionEvaluator {
+}
