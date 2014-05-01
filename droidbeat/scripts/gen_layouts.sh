3fb71cfa3d6507e49a2aa8af9b499152ffde6e91
Adam Erb <adam.l.erb@gmail.com>
2014-05-01 14:29:57 +0100

    Adding new record icon

--- droidbeat/scripts/gen_layouts.sh
+++ droidbeat/scripts/gen_layouts.sh
@@ -11,7 +11,7 @@ mkdir $SVG_FOLDER -p
 mkdir $LAYOUT_FOLDER -p
 mkdir $PNG_FOLDER -p
 
-ICONS=('paste' 'controls' 'folder' 'start' 'refresh' 'check' 'cross' 'add' 'copy' 'pencil')
+ICONS=('paste' 'controls' 'folder' 'start' 'refresh' 'check' 'cross' 'add' 'copy' 'pencil' 'record')
 for ICON in "${ICONS[@]}"
 do
 	echo Exporting $ICON
