30040f64590b8d4fea8f8116f72693898e4d3e14
Adam Erb <adam.l.erb@gmail.com>
2014-05-18 18:19:54 +0200

    Switching audio out def to 8 bit. Not sure what was going on there before ...

--- droidbeat/src/main/java/com/tasty/fish/android/media/audio/AndroidAudioDevice.java
+++ droidbeat/src/main/java/com/tasty/fish/android/media/audio/AndroidAudioDevice.java
@@ -5,7 +5,7 @@ import android.media.*;
 public class AndroidAudioDevice {
     // 44100,22050,11025,8000
     int sampleRate = 22050;
-    int format = AudioFormat.ENCODING_PCM_16BIT;
+    int format = AudioFormat.ENCODING_PCM_8BIT;
     int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
     AudioTrack _track;
 
