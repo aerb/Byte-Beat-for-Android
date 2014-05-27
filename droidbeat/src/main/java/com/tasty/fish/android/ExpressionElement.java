904405013cbcd4a0ba9f5cd53e5f7ea57c1ded71
Adam Erb <Adam@Lenovo-PC.(none)>
2014-05-27 01:23:18 -0700

    Adding select button for expression row.

--- droidbeat/src/main/java/com/tasty/fish/android/ExpressionElement.java
+++ droidbeat/src/main/java/com/tasty/fish/android/ExpressionElement.java
@@ -2,9 +2,9 @@ package com.tasty.fish.android;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,34 +27,54 @@ public class ExpressionElement extends RelativeLayout {
     }
 
     private void init(Context c){
-        setBackgroundColor(0xFFC83D);
-        TextView
-            text = new TextView(c);
-            text.setTextColor(Color.WHITE);
-            text.setText(_expression.getName());
-        addView(text);
-        LinearLayout
-            layout = new LinearLayout(c);
-            layout.setOrientation(LinearLayout.HORIZONTAL);
+        final int buttonLayoutId = 100;
+
+        int rowHeight = (int)PixelConverter.ToPixels(50);
+
+        setBackgroundColor(0xFF333333);
+        AbsListView.LayoutParams
+        elementLayoutParams = new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, rowHeight);
+        setLayoutParams(elementLayoutParams);
+        HighlightLayout
+            textLayout = new HighlightLayout(c);
+            textLayout.setGravity(CENTER_VERTICAL);
+            textLayout.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println(_expression.getName());
+                }
+            });
             RelativeLayout.LayoutParams
-                params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
-                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            layout.setLayoutParams(params);
-            int buttonSize = (int)PixelConverter.ToPixels(50);
+            textLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            textLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            textLayoutParams.addRule(RelativeLayout.LEFT_OF, buttonLayoutId);
+            textLayout.setLayoutParams(textLayoutParams);
+            TextView
+                text = new TextView(c);
+                text.setGravity(CENTER_VERTICAL);
+                text.setTextColor(Color.WHITE);
+                text.setTextSize(20);
+                text.setText(_expression.getName());
+            textLayout.addView(text);
+        addView(textLayout);
+        LinearLayout
+            buttonLayout = new LinearLayout(c);
+            buttonLayout.setId(buttonLayoutId);
+            buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
+            RelativeLayout.LayoutParams
+            buttonLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            buttonLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            buttonLayout.setLayoutParams(buttonLayoutParams);
             HighlightButton
                 edit = new HighlightButton(c);
                 edit.setImageResource(R.drawable.save);
-
-                edit.setLayoutParams(new ViewGroup.LayoutParams(buttonSize,buttonSize));
-                layout.addView(edit);
+                edit.setLayoutParams(new ViewGroup.LayoutParams(rowHeight,rowHeight));
+                buttonLayout.addView(edit);
             HighlightButton
                 delete = new HighlightButton(c);
                 delete.setImageResource(R.drawable.trash);
-                delete.setLayoutParams(new ViewGroup.LayoutParams(buttonSize,buttonSize));
-            layout.addView(delete);
-        addView(layout);
+                delete.setLayoutParams(new ViewGroup.LayoutParams(rowHeight,rowHeight));
+            buttonLayout.addView(delete);
+        addView(buttonLayout);
     }
-
-
-
 }
